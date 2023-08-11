96946a888a97c3f8d3ace45e0374f108b2efa682
Armando Liebenberg <Armandoliebenberg@protonmail.com>
2023-08-11 13:32:37 +1000

    Initialized Cmake, set up directories

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by aliebs on 11/08/23.
+//
+
+#include <cstdio>
+
+int main(int argc, char* argv[]) {
+    printf("Hello, world!\n");
+    return 0;
+}
---
+++ src/main.h
@@ -0,0 +1,8 @@
+//
+// Created by aliebs on 11/08/23.
+//
+
+#ifndef COLOURQUANTIZATION_MAIN_H
+#define COLOURQUANTIZATION_MAIN_H
+
+#endif //COLOURQUANTIZATION_MAIN_H
