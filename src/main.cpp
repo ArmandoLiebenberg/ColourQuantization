f6eeb6dadcc90b87d992e6dc6d067e90b90b4a11
Armando Liebenberg <34469720+ArmandoLiebenberg@users.noreply.github.com>
2023-09-07 17:18:32 +1000

    working version

--- src/main.cpp
+++ src/main.cpp
@@ -10,13 +10,10 @@
 #include <random>
 #include <functional>
 #include <chrono>
+#include <cfloat>
 
-#define NUM_KMEANS 8
-#define FAR 99999
-#define NUM_ITERATIONS 4
-
+#define NUM_KMEANS 3
 #define STB_IMAGE_WRITE_IMPLEMENTATION
-
 
 extern "C" {
     #define STB_IMAGE_IMPLEMENTATION
@@ -24,9 +21,194 @@ extern "C" {
     #include "../lib/stb_image_write.h"
 }
 
+bool load_image(std::vector<unsigned char>& image, const std::string& filename, int& x, int&y);
+std::vector<std::vector<int>> complete_random();
+std::vector<std::vector<int>> select_random(std::vector<unsigned char> image, int width, int height);
+std::vector<std::vector<int>> fuzzy(std::vector<unsigned char> image, int width, int height);
+
+
+int main()
+{
+    // -------------------------------------> Image Reading
+    std::string filename = "mountains.png";
+    
+    int width, height;
+    std::vector<unsigned char> image;
+    bool success = load_image(image, filename, width, height);
+    if (!success)
+    {
+        std::cout << "Error loading image\n";
+        return 1;
+    }
+    
+    std::cout << "Image width = " << width << '\n';
+    std::cout << "Image height = " << height << '\n';
+    
+    const size_t RGB = 3;
+
+    // -------------------------------------> Create tag vector
+    std::vector<int> tags(width*height, -1);
+
+    // -------------------------------------> generate random k means 
+
+    //std::vector<std::vector<int>> kmeans = complete_random();
+    std::vector<std::vector<int>> kmeans = select_random(image, width, height);
+    //std::vector<std::vector<int>> kmeans = fuzzy(image, width, height);
+
+/*
+    bool changed = true;
+    for (int k = 0; k < 1; k++) {
+        changed = false;
+
+        // For each pixel, assign to nearest cluster
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                size_t index = 3 * (x + y*width); // data is sequential in memory
+
+                double miniumum_distance = DBL_MAX;
+                int tag = -1;
+
+                for (int i = 0; i < NUM_KMEANS; i++) {
+                    double r = kmeans[i][0] - static_cast<int>(image[index + 0]);
+                    double g = kmeans[i][1] - static_cast<int>(image[index + 1]);
+                    double b = kmeans[i][2] - static_cast<int>(image[index + 2]);
+                    double distance = std::sqrt(r*r + g*g + b*b);
+
+                    if (distance < miniumum_distance) {
+                        miniumum_distance = distance;
+                        tag = i;
+                    }
+                }
+
+                //printf("pixel: %d  tag: %d\n", x + y*width, tag);
+                tags[x + y*width] = tag;
+            }
+        }
+
+        // Recompute the center of each cluster
+        for (int kmean = 0; kmean < NUM_KMEANS; kmean++) {
+
+            std::vector<double> mean {0.0, 0.0, 0.0};
+            int num = 0;
+
+            for(int pixel = 0; pixel < width*height; pixel++) {
+                if (tags[pixel] == kmean) {
+                    //printf("pixel data: %d, %d, %d\n", static_cast<int>(image[pixel + 0]), static_cast<int>(image[pixel + 1]), static_cast<int>(image[pixel + 2]));
+                    mean[0] += static_cast<int>(image[pixel + 0]);
+                    mean[1] += static_cast<int>(image[pixel + 1]);
+                    mean[2] += static_cast<int>(image[pixel + 2]);
+                    num += 1;
+                }
+            }
+
+            // find average. If no pixels, leave as orignal value
+            
+            if (num > 0) {
+                mean[0] = mean[0] / num;
+                mean[1] = mean[1] / num;
+                mean[2] = mean[2] / num;
+                printf("cluser: %d  rgb: (%g, %g, %g)\n", kmean, mean[0], mean[1], mean[2]);
+                kmeans[kmean] = std::vector<int>{static_cast<int>(std::floor(mean[0])), static_cast<int>(std::floor(mean[1])), static_cast<int>(std::floor(mean[2])), kmean};
+            }
+        }
+    }*/
+
+    int count = 0;
+    while (count < 15) {
+        //printf("--------- ITERATION %d ----------", count);
+        // for each vector:
+         // For each pixel, assign to nearest cluster
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                size_t index = 3 * (x + y*width); // data is sequential in memory
+
+                double miniumum_distance = DBL_MAX;
+                int tag = -1;
+
+                for (int i = 0; i < NUM_KMEANS; i++) {
+                    double r = kmeans[i][0] - static_cast<int>(image[index + 0]);
+                    double g = kmeans[i][1] - static_cast<int>(image[index + 1]);
+                    double b = kmeans[i][2] - static_cast<int>(image[index + 2]);
+                    double distance = std::sqrt(r*r + g*g + b*b);
+
+                    if (distance < miniumum_distance) {
+                        miniumum_distance = distance;
+                        tag = i;
+                    }
+                }
+
+                //printf("pixel: %d  tag: %d\n", x + y*width, tag);
+                tags[x + y*width] = tag;
+            }
+        }
+
+        // for each set of vectors belonging to a tag
+        // calculate a new mean based on the average location of those vectors
+        
+        for (int i = 0; i < NUM_KMEANS; i++) {
+            int num = 0;
+            std::vector<double> mean {0.0, 0.0, 0.0};
+            for (int y = 0; y < height; y++) {
+                for (int x = 0; x < width; x++) {
+                    size_t index = RGB * (x + y * width);
+                    if (tags[x + y*width] == i) {
+                        mean[0] += static_cast<int>(image[index + 0]);
+                        mean[1] += static_cast<int>(image[index + 1]);
+                        mean[2] += static_cast<int>(image[index + 2]);
+                        num += 1;
+                    }
+                }
+            }
+            if (num != 0) {
+                mean[0] /= num;
+                mean[1] /= num;
+                mean[2] /= num;
+            } else {
+                // If there are no matches we set it to the original value
+                mean[0] = kmeans[i][0];
+                mean[1] = kmeans[i][1];
+                mean[2] = kmeans[i][2];
+            }
+            kmeans[i] = std::vector<int>{static_cast<int>(mean[0]), static_cast<int>(mean[1]), static_cast<int>(mean[2]), i};
+
+        }
+        
+        count += 1;
+    }
+
+
+
+
+
+    // -----------------------> Modify image data
+    for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width; x++) {
+            size_t index = RGB * (x + y*width);
+            image[index + 0] = static_cast<unsigned char>(kmeans[tags[x + y*width]][0]);
+            image[index + 1] = static_cast<unsigned char>(kmeans[tags[x + y*width]][1]);
+            image[index + 2] = static_cast<unsigned char>(kmeans[tags[x + y*width]][2]);
+
+        }
+    }
+
+    // print all the tags
+    /*
+    for (int i = 0; i < width*height; i++) {
+        printf("%d ", tags[i]);
+    }
+    printf("\n");*/
+
+    unsigned char* data = &image.front();
+
+    remove("output.png");
+    stbi_write_png("output.png", width, height, RGB, data, width*RGB);
+
+    return 0;
+}
+
 // Loads as RGBA... even if file is only RGB
 // Feel free to adjust this if you so please, by changing the 4 to a 0.
-bool load_image(uint8_t* image, const std::string& filename, int& x, int&y)
+bool load_image(std::vector<unsigned char>& image, const std::string& filename, int& x, int&y)
 {
     int n;
     unsigned char* data = stbi_load(filename.c_str(), &x, &y, &n, 3);
@@ -39,7 +221,8 @@ bool load_image(uint8_t* image, const std::string& filename, int& x, int&y)
 }
 
 
-std::vector<std::vector<int>> complete_random() {
+std::vector<std::vector<int>> complete_random() 
+{
 
     // ------- generate random k means
     typedef std::chrono::high_resolution_clock myclock;
@@ -60,28 +243,39 @@ std::vector<std::vector<int>> complete_random() {
     return kmeans;
 }
 
-std::vector<std::vector<int>> select_random(std::vector<unsigned char> image, int width, int height) {
+std::vector<std::vector<int>> select_random(std::vector<unsigned char> image, int width, int height) 
+{
     // ------- generate random k means
-    typedef std::chrono::high_resolution_clock myclock;
+    /*typedef std::chrono::high_resolution_clock myclock;
     myclock::time_point beginning = myclock::now();
     myclock::duration time = myclock::now() - beginning;
     unsigned seed = time.count();
 
     std::mt19937 generator(seed);
     std::uniform_int_distribution<int> distribution(0,width*height);
-    auto dice = std::bind ( distribution, generator );
+    auto dice = std::bind ( distribution, generator );*/
+
+    // generate a random seed
+    std::random_device r;
+    std::seed_seq seed_seq{r(), r(), r(), r(), r(), r(), r(), r()};
+
+    // initialise the rng with the seed
+    std::mt19937 mersenne{seed_seq};
+
+    std::uniform_int_distribution<int> dice(0, width*height);
 
     // times three to get the start of a pixel RGB, our generator only does width*height
     std::vector<std::vector<int>> kmeans(NUM_KMEANS);
     for (int i = 0; i < NUM_KMEANS; i++) {
-        int index = dice() * 3;
-        printf("Index is: %d\n", index);
-        kmeans[i] = std::vector<int> {static_cast<int>(image[index + 0]), static_cast<int>(image[index + 0]), static_cast<int>(image[index + 0]), i};
+        int index = dice(mersenne) * 3;
+        printf("Index is: %d\n", index/2);
+        kmeans[i] = std::vector<int> {static_cast<int>(image[index + 0]), static_cast<int>(image[index + 1]), static_cast<int>(image[index + 2]), i};
     }
     return kmeans;
 }
 
-std::vector<std::vector<int>> fuzzy(std::vector<unsigned char> image, int width, int height) {
+std::vector<std::vector<int>> fuzzy(std::vector<unsigned char> image, int width, int height)
+{
     // ------- generate random k means
     typedef std::chrono::high_resolution_clock myclock;
     myclock::time_point beginning = myclock::now();
@@ -128,110 +322,3 @@ std::vector<std::vector<int>> fuzzy(std::vector<unsigned char> image, int width,
 
     return kmeans;
 }
-
-/*
-std::vector<std::vector<int>> fuzzy(std::vector<unsigned char> image) {
-
-    return kmeans;
-}*/
-
-int main()
-{
-    // -------------------------------------> Image Reading
-    std::string filename = "image_skull.png";
-    
-    int width, height;
-    uint8_t* image = new uint8_t[width * height * 3];
-    bool success = load_image(image, filename, width, height);
-    if (!success)
-    {
-        std::cout << "Error loading image\n";
-        return 1;
-    }
-    
-    std::cout << "Image width = " << width << '\n';
-    std::cout << "Image height = " << height << '\n';
-    
-    const size_t RGB = 3;
-
-    // -------------------------------------> Create tag vector
-    std::vector<int> tags(width*height, -1);
-
-    // ------- generate random k means
-    //std::vector<std::vector<int>> kmeans = complete_random();
-    //std::vector<std::vector<int>> kmeans = select_random(image, width, height);
-    std::vector<std::vector<int>> kmeans = fuzzy(image, width, height);
-
-    int count = 0;
-    while (count < NUM_ITERATIONS) {
-        //printf("--------- ITERATION %d ----------", count);
-        // for each vector:
-        for (int x = 0; x < width; x++) {
-            for (int y = 0; y < height; y++) {
-                size_t index = RGB * (y * width + x);
-                //printf("x:%d, y:%d, %d %d %d\n", x, y, static_cast<int>(image[index + 0]), static_cast<int>(image[index + 1]), static_cast<int>(image[index + 2]));
-
-                // find the closest k means
-                double longestDistance = FAR;
-                int longestTag = -1;
-                for (int i = 0; i < NUM_KMEANS; i++) {
-                    double distance = std::sqrt(pow((kmeans[i][0] - static_cast<int>(image[index + 0])),2) + pow((kmeans[i][1] 
-                        - static_cast<int>(image[index + 1])),2) + pow((kmeans[i][2] - static_cast<int>(image[index + 2])),2));
-                    //printf("%g\n", distance);
-                    if (distance < longestDistance) {
-                        longestDistance = distance;
-                        longestTag = kmeans[i][3];
-                    }
-                }
-                // tag the vector with that k means tag
-                if (longestDistance != FAR) {
-                    tags[y*height + x] = longestTag;
-                    //printf("Closest kmeans of vector is %d\n", tags[y*height + x]);
-                }
-
-            }
-            
-        }
-        // for each set of vectors belonging to a tag
-        // calculate a new mean based on the average location of those vectors
-        int num = 0;
-        for (int i = 0; i < NUM_KMEANS; i++) {
-            std::vector<double> mean {0.0, 0.0, 0.0};
-            for (int x = 0; x < width; x++) {
-                for (int y = 0; y < height; y++) {
-                    size_t index = RGB * (y * width + x);
-                    if (tags[y*height + x] == i) {
-                        mean[0] += static_cast<int>(image[index + 0]);
-                        mean[1] += static_cast<int>(image[index + 1]);
-                        mean[2] += static_cast<int>(image[index + 2]);
-                        num += 1;
-                    }
-                }
-            }
-            if (num != 0) {
-                mean[0] /= num;
-                mean[1] /= num;
-                mean[2] /= num;
-            } else {
-                // If there are no matches we set it to the original value
-                mean[0] = kmeans[i][0];
-                mean[1] = kmeans[i][1];
-                mean[2] = kmeans[i][2];
-            }
-            kmeans[i] = std::vector<int>{static_cast<int>(mean[0]), static_cast<int>(mean[1]), static_cast<int>(mean[2]), i};
-
-        }
-        count += 1;
-    }
-
-    // Print all the tags
-    for (int i = 0; i < width*height; i++) {
-        //printf("%d ", tags[i]);
-    }
-    printf("\n");   
-
-    stbi_write_jpg("output.hdr", width, height, 3, &image, 100);
-
-    return 0;
-}
-
---
+++ src/write.cpp
@@ -0,0 +1,51 @@
+#include <cstdio>
+#include <iostream>
+#include <vector>
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+
+extern "C" {
+    #define STB_IMAGE_IMPLEMENTATION
+    #include "../lib/stb_image.h"
+    #include "../lib/stb_image_write.h"
+}
+
+// Loads as RGBA... even if file is only RGB
+// Feel free to adjust this if you so please, by changing the 4 to a 0.
+bool load_image(std::vector<unsigned char>& image, const std::string& filename, int& x, int&y)
+{
+    int n;
+    unsigned char* data = stbi_load(filename.c_str(), &x, &y, &n, 4);
+    if (data != nullptr)
+    {
+        image = std::vector<unsigned char>(data, data + x * y * 4);
+    }
+    stbi_image_free(data);
+    return (data != nullptr);
+}
+
+int main()
+{
+    // -------------------------------------> Image Reading
+    std::string filename = "image_skull.png";
+    
+    int width, height;
+    std::vector<unsigned char> image;
+    bool success = load_image(image, filename, width, height);
+    if (!success)
+    {
+        std::cout << "Error loading image\n";
+        return 1;
+    }
+    
+    std::cout << "Image width = " << width << '\n';
+    std::cout << "Image height = " << height << '\n';
+    
+    const size_t RGB = 3;
+
+    unsigned char* data = &image.front();
+
+    stbi_write_png("output.png", width, height, 4, data, width*4);
+
+    return 0;
+}
